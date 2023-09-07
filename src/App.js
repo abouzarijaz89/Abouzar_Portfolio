8942ddf6ae4201e56d937024f12e0b2c176d4bd7
abouzarijaz89 <abouzarijaz89@gmail.com>
2023-09-07 18:03:17 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import Aos from "aos";
+
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Hero from "./components/UI/Hero";
+import Services from "./components/UI/Services";
+import "./App.css";
+
+function App() {
+  useEffect(() => {
+    Aos.init();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <main>
+        <Hero />
+        <Services />
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/UI/Services.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <section id="services">
+        <div className="container lg:pt-5">
+            <div className="text-center">
+                <h2 className='text-headingColor font-[800] text-[2.4rem] mb-5'>What do I do</h2>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Services
